2c666842a3bdffe8fad4447d1bf24f10fed5e3e1
Stijn Hellem <stijn.hellem@student.howest.be>
2020-06-17 11:18:40 +0200

    directory structure adjusted, added containers and moved the routes to content file

--- client/src/containers/content/Home/index.js
+++ client/src/containers/content/Home/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 //import { Link } from "react-router-dom";
 //import PropTypes from "prop-types";
-import { useStore } from "../../hooks";
+import { useStore } from "../../../hooks";
+
 
 const Home = () => {
   const { uiStore, clanMemberStore } = useStore()
   return (
     <>
    <p>hello world</p>
+
    <ul >
           
           {clanMemberStore.clanMembers.map(clanMember => ( 
---
+++ client/src/containers/header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import { Link } from "react-router-dom";
+//import PropTypes from "prop-types";
+
+
+const Header = () => {
+  return (
+   <header>Hier komt de header</header>
+   
+  );
+};
+
+Header.propTypes = {
+
+};
+
+export default Header;
