c009617159ac48dfae0626e54dfef9c9a1f9eb4d
Sander Devacht <sander.devacht@student.howest.be>
2020-06-18 10:38:07 +0200

    #66 button algemeen

--- client/src/components/buttons/Algemeen/TheePotFlow.js
+++ client/src/components/buttons/Algemeen/TheePotFlow.js
@@ -1,22 +1,20 @@
 import React from "react";
-import styles from "./AlgemeenButton.module.css"
+import styles from "./TheePotLink.module.css"
 import { Link } from "react-router-dom";
 import { ROUTES } from "../../../consts";
 
 import { ReactComponent as TheePot } from './btnTheePot.svg';
 
 
-const ButtonAlgemeen = ({text, linkTo}) => {
+const TheePotFlow = ({text}) => {
   return (
     <div className={styles}>
-    <Link  to={linkTo}>
       <TheePot/>
       <p className={styles.button_title}>{text}</p>
-    </Link>
   </div>
   );
 };
 
-export default ButtonAlgemeen;
+export default TheePotFlow;
 
 
---
+++ client/src/components/buttons/Algemeen/TheePotLink.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./TheePotLink.module.css"
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../../consts";
+
+import { ReactComponent as TheePot } from './btnTheePot.svg';
+
+
+const TheePotLink = ({text, linkTo}) => {
+  return (
+    <div className={styles}>
+    <Link  to={linkTo}>
+      <TheePot/>
+      <p className={styles.button_title}>{text}</p>
+    </Link>
+  </div>
+  );
+};
+
+export default TheePotLink;
+
+
