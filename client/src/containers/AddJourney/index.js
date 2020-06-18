b66a77929f331c3ae950df77294e526064f7460f
Stijn Hellem <stijn.hellem@student.howest.be>
2020-06-18 11:11:15 +0200

    copied data to addJourney flow

--- client/src/containers/AddJourney/index.js
+++ client/src/containers/AddJourney/index.js
@@ -11,15 +11,51 @@ import StartJourneyButton from "../../components/buttons/StartJourney/StartJourn
 import MyclanButton from "../../components/buttons/MyClan/MyclanButton"
 import BackToWorldButton from "../../components/buttons/BackToWorld/BackToWorldButton"
 import { ROUTES } from "../../consts"
-
-
+import ClanMemberStore from "../../stores/ClanMemberStore";
+import { useStore } from "../../hooks";
 const AddJourney = () => {
+  const { uiStore, roleStore, clanStore, clanMemberStore } = useStore()
+  console.log(clanMemberStore);
+  if(uiStore.currentClan){
+        console.log("current lcan")
+  }else{
+        console.log("no current clan")
+        uiStore.setCurrentClan(clanStore.resolveClan("675a4afd-7810-4666-a90b-bdabee51b103"));
+  }
   return (
 
     <>
    <p>AddJourney</p>
 
+   <ul >
+          Dit zijn alle clans uit de db: 
+          {clanStore.clans.map(clan  => (     
+                <li>{clan.name}</li>
+          ))}
+  </ul>
+ 
+  <ul >
+          Dit zijn alle role names uit de db: 
+          {roleStore.roles.map(role => (     
+                <li>{role.roleName}</li>
+          ))}
+  </ul>
+  <ul>
+          
+          {clanMemberStore.clanMembers.map(clanMember => (     
+                <li>Dit zijn alle clanMembers: {clanMember.name}</li>
+          ))}
+  </ul>
 
+
+      
+  <p>Dit is de current clan name: {uiStore.currentClan.name}</p>
+  <ul>
+          Dit zijn alle clanmembers in de current clan: 
+          {uiStore.currentClan.clanMembers.map(clanMember => (     
+                <li> {clanMember.name}</li>
+          ))}
+  </ul>
     
     
     <PageTitle title={"Who joins the journey?"} subtext={"dit is een zin voor in de subtext veel plezier ermee"}/>
