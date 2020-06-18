397a51ed1c0159fc4717116d7a00cffef5ba00d3
vanessabloes <vanessa.bloes@student.howest.be>
2020-06-18 12:17:52 +0200

    part 1 core flow: from loading page to core page #83

--- client/src/containers/AddStory/index.js
+++ client/src/containers/AddStory/index.js
@@ -7,16 +7,49 @@ import Loading from "../../components/Loading";
 import Core from "../../components/Core";
 import Challenge from "../../components/Challenge";
 
+import { useStore } from "../../hooks";
+import TheePotFlow from "../../components/buttons/Algemeen/TheePotFlow";
+import TheePotLink from "../../components/buttons/Algemeen/TheePotLink";
+import { ROUTES } from "../../consts";
 
 // conditional over alle drie de elementen:
 // core_loading, core_wheel, core_challenge
 // setState in de button steken (per pagina) om de juiste componenten te laden, deze setState/setPage bijhouden in store
 const AddStory = () => {
+
+  const { coreStore } = useStore();
   return useObserver(() => (
     <>
-      <Loading />
-      <Core />
-      <Challenge />
+      {coreStore.state === "loading" ?
+
+        <div>
+          <Loading />
+          <button
+            value="core"
+            onClick={e => coreStore.setState(e.target.value)}>
+            <TheePotFlow text="Start storytelling" />
+          </button>
+        </div>
+        :
+
+       
+            <div>
+              <div>
+                <Core />
+                <button>
+                  <TheePotFlow text="Find your way back" />
+                </button>
+                <TheePotLink text="Add Story" linkTo={ROUTES.path} />
+              </div>
+
+              :
+
+            <div>
+                <Challenge />
+                <TheePotLink text="We're back on track" linkTo={ROUTES.path} />
+              </div>
+            </div>}
+
     </>
   ));
 };
