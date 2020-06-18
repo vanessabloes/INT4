9c2e4c9332df7cab8c619598637a25c4bc9cf4d0
Stijn Hellem <stijn.hellem@student.howest.be>
2020-06-18 23:15:53 +0200

    added some drag and drop tests with react DND

--- client/src/components/ChooseRoles/index.js
+++ client/src/components/ChooseRoles/index.js
@@ -5,17 +5,43 @@ import { useStore } from "../../hooks";
 import { useObserver } from "mobx-react-lite";
 import { STATES } from "../../consts";
 import TheePotFlow from "../buttons/Algemeen/TheePotFlow";
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
-
+import Dustbin from "../Dustbin";
+import Box from "../Box";
 
 const ChooseRoles = () => {
   const { uiStore, roleStore, clanStore, clanMemberStore } = useStore()
  
- 
+
+
+
   return useObserver (() => (
     <>
    <p>choose roles</p>
+   <div className="App">
+				<DndProvider backend={HTML5Backend}>
+        <div>
+    <div style={{ overflow: 'hidden', clear: 'both', margin: '-1rem' }}>
+      <Dustbin greedy={true}>
+        <Dustbin greedy={true}>
+          <Dustbin greedy={true} />
+        </Dustbin>
+      </Dustbin>
+      <Dustbin>
+        <Dustbin>
+          <Dustbin />
+        </Dustbin>
+      </Dustbin>
+    </div>
 
+    <div style={{ overflow: 'hidden', clear: 'both', marginTop: '1.5rem' }}>
+      <Box />
+    </div>
+  </div>
+				</DndProvider>
+			</div>
    <TheePotFlow text={"Explore roles"} onClick={e => uiStore.setAddJourneyState(STATES.ADDJOURNEY_STATE_EXPLOREROLES)}/>
 
   </>
