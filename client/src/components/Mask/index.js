802b70b159006945fd153afbfdf177720d82e608
pepijndejonckheeree <pepijn.de.jonckheere@student.howest.be>
2020-06-22 11:57:05 +0200

    myclan page styled

--- client/src/components/Mask/index.js
+++ client/src/components/Mask/index.js
@@ -8,14 +8,14 @@ const Mask = ({clanMember}) => {
     const { topMaskStore, middleMaskStore, bottomMaskStore } = useStore();
 
     return useObserver(() => (
-        <>
         <div className={styles.maskContainer}>
-            <img className={styles.maskTop} src={topMaskStore.resolveTopMask(clanMember.topMaskId).topImage}/>
-            <img className={styles.maskMiddle} src={middleMaskStore.resolveMiddleMask(clanMember.middleMaskId).middleImage}/>
-            <img className={styles.maskBottom} src={bottomMaskStore.resolveBottomMask(clanMember.bottomMaskId).bottomImage}/>
+            <div className={styles.maskWrapper}>
+                <img className={styles.maskTop} src={topMaskStore.resolveTopMask(clanMember.topMaskId).topImage}/>
+                <img className={styles.maskMiddle} src={middleMaskStore.resolveMiddleMask(clanMember.middleMaskId).middleImage}/>
+                <img className={styles.maskBottom} src={bottomMaskStore.resolveBottomMask(clanMember.bottomMaskId).bottomImage}/>
+            </div>
+            <p className={styles.name}>{clanMember.name}</p>
         </div>
-        <p>{clanMember.name}</p>
-        </>
     ));
 };
 
