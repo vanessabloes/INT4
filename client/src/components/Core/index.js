bab84ececc84c7c67c70c8e8e31b3dbefb1585c7
vanessabloes <vanessa.bloes@student.howest.be>
2020-06-24 08:00:11 +0200

    added manual button

--- client/src/components/Core/index.js
+++ client/src/components/Core/index.js
@@ -14,110 +14,121 @@ import PageTitle from "../PageTitle/PageTitle";
 import { useParams } from "react-router-dom";
 import ProgressFlame from "../ProgressFlames";
 
+import styles from "./Core.module.css";
+
 
 
 
 const Core = () => {
 
-    const { uiStore, journeyStore, storyStore } = useStore();
-    const { id, storyId } = useParams();
-    const STATE_LOADING = 'loading';
-    const STATE_NOT_FOUND = 'notFound';
-    const STATE_LOADING_MORE_DETAILS = 'loading more details';
-    const STATE_FULLY_LOADED = 'fully loaded';
+  const [manual, setManual] = useState(false);
 
-    const showOverlay = () => {
+  const showManual = () => {
+    setManual(true);
+  }
 
-    }
-    const [story, setStory] = useState(storyStore.resolveStory(storyId));
-    const [state, setState] = useState(STATE_LOADING);
-  
+  const { uiStore, journeyStore, storyStore } = useStore();
+  const { id, storyId } = useParams();
+  const STATE_LOADING = 'loading';
+  const STATE_NOT_FOUND = 'notFound';
+  const STATE_LOADING_MORE_DETAILS = 'loading more details';
+  const STATE_FULLY_LOADED = 'fully loaded';
 
-  
-    useEffect(() => {
-  
-      const loadStory = async (id, storyId) => {
-  
+  const showOverlay = () => {
+
+  }
+  const [story, setStory] = useState(storyStore.resolveStory(storyId));
+  const [state, setState] = useState(STATE_LOADING);
+
+
+
+  useEffect(() => {
+
+    const loadStory = async (id, storyId) => {
+
       //  try{
-          console.log(id)
-          console.log(storyId)
- 
-          const story = await storyStore.loadStory(storyId);
-          const journey = await journeyStore.loadJourney(id);
-          
-          await uiStore.setCurrentJourney(journey);
-          await uiStore.setCurrentStory(story);
-          await storyStore.loadDefinedStoryWordsForStory(storyId);
-          console.log(story)
-          console.log(journey)
-          if (!story) {
-            setState(STATE_NOT_FOUND);
-            return;
-          }
-          
-          setState(STATE_LOADING_MORE_DETAILS);
-         // await storyStore.loadDefinedStoryWordsForStory(storyId);
-          //const generatedName = `from `+  story.definedStoryWords[0].content + ` to ` + story.definedStoryWords[story.definedStoryWords.length - 1].content;
-         // await story.updateFromJson({
-         //   name: generatedName
-         // });
-         // console.log(story);
-         // await story.update();
-          setStory(story);
-          await journeyStore.loadWayfarersForJourney(id);
-          await storyStore.loadAllSpokenNounsForStory(storyId);
-          
+      console.log(id)
+      console.log(storyId)
 
-        
-           setState(STATE_FULLY_LOADED);
+      const story = await storyStore.loadStory(storyId);
+      const journey = await journeyStore.loadJourney(id);
+
+      await uiStore.setCurrentJourney(journey);
+      await uiStore.setCurrentStory(story);
+      await storyStore.loadDefinedStoryWordsForStory(storyId);
+      console.log(story)
+      console.log(journey)
+      if (!story) {
+        setState(STATE_NOT_FOUND);
+        return;
+      }
+
+      setState(STATE_LOADING_MORE_DETAILS);
+      // await storyStore.loadDefinedStoryWordsForStory(storyId);
+      //const generatedName = `from `+  story.definedStoryWords[0].content + ` to ` + story.definedStoryWords[story.definedStoryWords.length - 1].content;
+      // await story.updateFromJson({
+      //   name: generatedName
+      // });
+      // console.log(story);
+      // await story.update();
+      setStory(story);
+      await journeyStore.loadWayfarersForJourney(id);
+      await storyStore.loadAllSpokenNounsForStory(storyId);
+
+
+
+      setState(STATE_FULLY_LOADED);
       //  }
-       
+
       //    catch (error) {
       //      console.log("error")
       //      setState(STATE_NOT_FOUND);
       //    }
-      }
-      loadStory(id, storyId);
-    }, [id, storyId, storyStore, journeyStore, setStory]);
-  
-  
+    }
+    loadStory(id, storyId);
+  }, [id, storyId, storyStore, journeyStore, setStory]);
 
 
-    return useObserver(() => {
-        if (state === STATE_NOT_FOUND) {
-          return <p>Story not found"</p>;
-        }
-        if (state === STATE_LOADING) {
-          return <p>Loading"</p>;;
-        }
-        return (
-        <>
-           <PageTitle title={"Your story"}/>
-            <ProgressFlame />
-           
-            <ul>
-            {uiStore.currentJourney.wayfarers.map(wayfarer => (
-              
-                    <li key={wayfarer.id} onClick={showOverlay} >{wayfarer.id}</li>
-                    
-                       
-               
-            ))}
-             </ul>
 
-            <Wordwheel />
-            <Manual />
-            <button
-                value="challenge"
-                onClick={e => uiStore.setAddStoryState(STATES.ADDSTORY_STATE_CHALLENGE)}>
-                <TheePotFlow
-                    text="Find your way back" />
-            </button>
-            <TheePotLink text="Add Story" linkTo={ROUTES.path} />
-        </>
-       );
-    });
-  };
+
+  return useObserver(() => {
+    if (state === STATE_NOT_FOUND) {
+      return <p>Story not found"</p>;
+    }
+    if (state === STATE_LOADING) {
+      return <p>Loading"</p>;;
+    }
+    return (
+      <>
+        <PageTitle title={"Your story"} />
+        <ProgressFlame />
+
+        <ul>
+          {uiStore.currentJourney.wayfarers.map(wayfarer => (
+
+            <li key={wayfarer.id} onClick={showOverlay} >{wayfarer.id}</li>
+
+
+
+          ))}
+        </ul>
+
+        <Wordwheel />
+
+        <button className={styles.manualButton} onClick={showManual}> <img className={styles.manualArrow} alt="icon of down arrow" src="/assets/img/BUTTONS/downarr.svg" /><p className={styles.manualButtonText}>How to tell a story?</p></button>
+        {manual ? <Manual /> : ""}
+
+        <button
+          value="challenge"
+          onClick={e => uiStore.setAddStoryState(STATES.ADDSTORY_STATE_CHALLENGE)}>
+          <TheePotFlow
+            text="Find your way back" />
+        </button>
+        <TheePotLink text="Add Story" linkTo={ROUTES.path} />
+      </>
+    );
+  });
+};
 
 
 export default Core;
